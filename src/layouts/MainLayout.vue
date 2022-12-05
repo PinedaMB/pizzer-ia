5135df5f54878ff0110ace15603be6788a85fd22
PinedaMB <mendez-pineda@outlook.es>
2022-12-04 21:45:10 -0500

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="q-px-xl bg-red" elevated>
+      <q-toolbar class="q-py-lg" id="toolbar">
+        <q-toolbar-title id="title">
+          <q-icon name="local_pizza" />
+          PIZZER-IA
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+
+      <div class="column items-center full-width q-gutter-y-md q-my-auto">
+        <df-messenger id="dialogflow" intent="WELCOME" chat-title="¿Qué desea ordenar?" agent-id="6ac9e96a-168a-448d-9153-5e9ea552ad92"
+          language-code="es"></df-messenger>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, onMounted } from "vue";
+</script>
+
+<style lang="scss">
+#title {
+  font-size: 1.8rem;
+  font-weight: 800;
+}
+
+q-header {
+  z-index: 1;
+}
+
+#toolbar {
+  padding-left: 10rem;
+  padding-right: 10rem;
+}
+
+#dialogflow {
+margin-top: 10rem;
+z-index: 1000000;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="landing" class="bg-red flex">
+    <div id="content">
+      <span id="sub" class="text-yellow">Eat pizza everyday</span>
+      <span id="full-title" class="text-white">Share your love for pizza</span>
+    </div>
+    <div id="image">
+      <img src="../assets/pizza-full.png" alt="pizza" />
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+#landing {
+  height: 87.3vh;
+  width: 100vw;
+}
+#content {
+  overflow: hidden;
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 15rem;
+  #sub {
+    font-size: 1.8rem;
+    font-weight: 800;
+  }
+  #full-title {
+    font-size: 90px;
+    font-weight: 900;
+    line-height: 1.2;
+    text-transform: uppercase;
+  }
+}
+#image {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-right: 15rem;
+}
+</style>
